e280266737149af7dc481ec0a777a88b506e20a8
farid manafov <faridbardium@gmail.com>
2018-06-21 16:34:55 +0000

    New tweets are now prepended into the html page through JS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
+const tweetData = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -18,7 +19,37 @@ const tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
+]
 
 function createTweetElement(objectOfTweets){
     let $tweet = `
@@ -37,13 +68,14 @@ function createTweetElement(objectOfTweets){
     return $tweet;
 }
 
-function renderTweets(tweetObject){
+function renderTweets(arrayOfTweets){
 
-    tweetObject.forEach(function(individualTweets){
-        $newTweet = createTweetElement(individualTweets)
-        $(".existing-tweets").append(newTweet)
-    })
-
+  arrayOfTweets.forEach(function(individualTweet){
+    let $newTweet = createTweetElement(individualTweet)
+    $(".tweet-container").prepend($newTweet)
+  })
 }
 
-renderTweets(tweetData);
+$(tweetData).ready(function(){
+  renderTweets(tweetData);
+});
